f5b58e90d11223b6acdb89014896f57139fbc79c
neko-nix <rodrigo.pereira.riquelme@gmail.com>
2026-02-03 23:27:32 -0300

    V2.0: Se mejora script de registrar movimiento para mejor acomodarse al broker nuevo. Se agrega verificación de la existencia de la tabla para el cache de los datos de conversión de monedas. Se arregla bug en rebalanceo de avantis, donde sólo calculaba el rebalanceo con los valores iniciales en vez de los actuales

--- src/core/rebalancearAvantis.py
+++ src/core/rebalancearAvantis.py
@@ -16,41 +16,69 @@ df = pd.read_csv(holdingsACWI, skiprows=9)
 proporcionUSA = (df[df['Location'] == 'United States']['Weight (%)'].sum()/100)
 
 proporcionesTickers = [
-    ["AVUS", 0.44],
-    ["AVUV", 0.22],
-    ["AVDE", 0.16],
-    ["AVDV", 0.08],
-    ["AVEM", 0.10]
+    ["AVUS", 0.4227],
+    ["AVUV", 0.2113],
+    ["AVDE", 0.1693],
+    ["AVDV", 0.0847],
+    ["AVEM", 0.1120]
 ]
 columnas = ["ticker", "proporción_ideal"]
 
 dfProporcionesIdeales = pd.DataFrame(proporcionesTickers, columns=columnas)
-print(dfProporcionesIdeales.head())
 
-#print("Las proporciones objetivo son las siguientes:")
+
+# Creamos una versión formateada para mostrar
+tabla_porcentajes = [[t, f"{p:.2%}"] for t, p in proporcionesTickers]
+
+print(tabulate(tabla_porcentajes,
+               headers=["Ticker", "Prop Ideal"],
+               tablefmt="rounded_grid",
+               numalign='right',
+               stralign='right'))
+
 
 
     
 def calcular_proporciones():
     conn = sqlite3.connect(DB_PATH)
-    query = "SELECT ticker, SUM(cantidad) as cantidad, SUM(total_clp) as inversion_total_clp, SUM(total_usd) as inversion_total_usd FROM transacciones GROUP BY ticker"
+    query = "SELECT ticker, SUM(cantidad) as cantidad, \
+            SUM(total_clp) as inversion_inicial_clp, \
+            SUM(total_usd) as inversion_inicial_usd  \
+            FROM transacciones GROUP BY ticker"
     activos = pd.read_sql_query(query, conn)
     conn.close()
+
+    # Inveriones inciales 
+    totalInicialCLP = activos['inversion_inicial_clp'].sum()
+    totalInicialUSD = activos['inversion_inicial_usd'].sum()
+
+    # Valores actuales
+    lista_tickers = activos['ticker'].unique().tolist()
+    precios_data = yf.download(lista_tickers, period="1d", progress=False)['Close']
+    precios_actuales = precios_data.iloc[-1]
+
+    activos['precio_actual_usd'] = activos['ticker'].map(precios_actuales)
+    activos['precio_actual_usd'] = activos['precio_actual_usd'].fillna(1)
+    activos['inversion_actual_usd'] = activos['precio_actual_usd'] * activos['cantidad']
     
 
-    totalActualCLP = activos['inversion_total_clp'].sum()
-    totalActualUSD = activos['inversion_total_usd'].sum()
+    totalActualUSD = activos['inversion_actual_usd'].sum()
 
-    activos['porcentaje_actual'] = activos["inversion_total_clp"]/totalActualCLP
-
-    activos = pd.merge(activos,dfProporcionesIdeales, on="ticker", how="outer").fillna(0) 
     dolarObservado = conversiones.get_usd()
-    
-    montoInvertirCLP = 560e3
-    comisionBroker = max((conversiones.get_usd()*0.49)/100, 5)
-    montoInvertirUSD = montoInvertirCLP/(dolarObservado+comisionBroker)
+    totalActualCLP = activos['inversion_actual_usd'].sum()*dolarObservado
 
+    # Porcentajes
+    activos['porcentaje_actual'] = activos["inversion_actual_usd"]/totalActualUSD
+    activos = pd.merge(activos,dfProporcionesIdeales, on="ticker", how="outer").fillna(0) 
+
+
+    # Montos a invertir
+    montoInvertirCLP = 560e3
+    comisionBroker = max((dolarObservado*0.49)/100, 5)
+    dolarBroker = comisionBroker + dolarObservado
+    montoInvertirUSD = montoInvertirCLP/(dolarObservado+comisionBroker)
     comisionPagada = montoInvertirUSD*comisionBroker
+    proporcionComision = (dolarBroker-dolarObservado)/dolarObservado
 
     print(f"Precio Dólar Mercado: ${dolarObservado:,.2f}")
     print(f"Precio Dólar Broker: ${dolarObservado+comisionBroker:,.2f}")   
@@ -59,34 +87,30 @@ def calcular_proporciones():
     print(f"Comisión total pagada por cambio de divisa: ${comisionPagada:,.2f} CLP, ${comisionPagada/dolarObservado:,.2f} USD")
 
 
+    # Inversión ideal
     activos['inversion_ideal'] = (totalActualUSD+montoInvertirUSD)*activos['proporción_ideal']
-    activos['inversion_ideal'] = activos['inversion_ideal'] - activos['inversion_total_usd']
+    activos['inversion_ideal'] = activos['inversion_ideal'] - activos['inversion_actual_usd']
     activos['inversion_ideal'] = activos['inversion_ideal'].clip(lower=0)
 
     activos['prop_invertir'] = activos['inversion_ideal'] / activos['inversion_ideal'].sum()
 
     # Esto se hace para que el monto a invertir coincida con la plata que vamos a inyectar de verdad
     activos['monto_invertir'] = montoInvertirUSD*activos['prop_invertir']
-    activos['comisión_pagada'] = (activos['monto_invertir'] / activos['monto_invertir'].sum()) * (comisionPagada/dolarObservado)
+    activos['comisión_pagada'] = activos['monto_invertir']*proporcionComision
 
-    lista_tickers = activos['ticker'].unique().tolist()
-    precios_data = yf.download(lista_tickers, period="1d", progress=False)['Close']
-    precios_actuales = precios_data.iloc[-1]
+    #activos['cantidad_comprar'] = activos['monto_invertir']/activos["precio_actual_usd"]
 
-    activos['precio_actual_usd'] = activos['ticker'].map(precios_actuales)
-    activos['precio_actual_usd'] = activos['precio_actual_usd'].fillna(1)
-    activos['cantidad_comprar'] = activos['monto_invertir']/activos["precio_actual_usd"]
-
-    activos['inversión_nueva'] = activos['inversion_total_usd']+(activos['cantidad_comprar']*activos['precio_actual_usd'])
+    # Cantidades nuevas estimadas
+    activos['inversión_nueva'] = activos['inversion_actual_usd']+activos['monto_invertir']
     activos['proporción_nueva'] = activos['inversión_nueva'] / activos['inversión_nueva'].sum()
 
-    totales = ['inversion_total_usd', 'porcentaje_actual', 'monto_invertir', 'comisión_pagada', 'inversión_nueva', 'proporción_nueva', 'proporción_ideal']
+    totales = ['inversion_actual_usd', 'porcentaje_actual', 'monto_invertir', 'comisión_pagada', 'inversión_nueva', 'proporción_nueva', 'proporción_ideal']
 
     activos.loc['TOTAL', totales] = activos[totales].sum()
     activos.loc['TOTAL', 'ticker'] = 'TOTAL'
     activos.loc['TOTAL'] = activos.loc['TOTAL'].fillna(0)
 
-    activos['inversion_total_usd']= activos['inversion_total_usd'].map("$ {:,.2f}".format)
+    activos['inversion_actual_usd']= activos['inversion_actual_usd'].map("$ {:,.2f}".format)
     activos['inversión_nueva']= activos['inversión_nueva'].map("$ {:,.2f}".format)
     activos['porcentaje_actual']= activos['porcentaje_actual'].map("{:,.2%}".format)
     activos['proporción_nueva']= activos['proporción_nueva'].map("{:,.2%}".format)
@@ -95,10 +119,16 @@ def calcular_proporciones():
     activos['comisión_pagada']= activos['comisión_pagada'].map("$ {:,.2f}".format)
 
 
+    orden_deseado = ["AVUS", "AVUV", "AVDE", "AVDV", "AVEM", "TOTAL"]
+
+    activos['ticker'] = pd.Categorical(activos['ticker'], categories=orden_deseado, ordered=True)
+
+    activos = activos.sort_values('ticker')
+
 
     print(tabulate(
         activos[['ticker',
-                'inversion_total_usd',
+                'inversion_actual_usd',
                 'porcentaje_actual',
                 'monto_invertir',
                 'comisión_pagada',
@@ -110,10 +140,10 @@ def calcular_proporciones():
                  'Prop Actual',
                  'Monto Invertir',
                  'Comisión',
-                 'Total Nuevp',
+                 'Total Nuevo',
                  'Prop Nueva',
                  'Prop Ideal'], 
-        tablefmt='fancy_grid',
+        tablefmt='rounded_grid',
         numalign='right',
         stralign='right',
         showindex=False
